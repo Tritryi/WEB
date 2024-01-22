051b93138762419bd4a92801f418359890a4bcbd
Luckyle <136337096+1Luckyle@users.noreply.github.com>
2024-01-23 00:10:04 +0100

    Add files via upload

---
+++ S1.05-06 V2/index.js
@@ -0,0 +1,22 @@
+var icon = document.getElementById("icon");
+
+      icon.onclick = function(){
+        document.body.classList.toggle("dark-theme");
+        if(document.body.classList.contains("dark-theme")){
+          icon.src = "./Images/sun.png";
+        }else{
+          icon.src = "./Images/moon.png";
+        }
+      }
+
+      function afficherDiv(id) {
+        var divObj = document.querySelectorAll('.obj');
+        divObj.forEach(function (divObj) {
+            divObj.style.display = 'none';
+        });
+    
+        var divShow = document.getElementById(id);
+        if (divShow) {
+            divShow.style.display = 'block';
+        }
+    }
